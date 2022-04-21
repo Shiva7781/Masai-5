d549a703174a04107605c43463de7f08fd596162
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-04-21 05:52:43 +0530

    .

--- d4_state_management/src/components/TodoItem.jsx
+++
@@ -1,9 +0,0 @@
-export const TodoItem = ({ todo, handleStatus }) => {
-  return (
-    <div>
-      {/* <div>id:{todo.id}</div> */}
-      {todo.title} - {todo.status ? "Done" : "Not Done"}{" "}
-      <button onClick={() => handleStatus(todo.id)}>Toggle</button>
-    </div>
-  );
-};
